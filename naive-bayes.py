19aaa58ca6544bbc64db5a1d5c7f208b8514d64d
ykp <ykp@ykps-MacBook-Pro.local>
2016-11-28 03:10:15 -0800

    naive-bayes

---
+++ naive-bayes.py
@@ -0,0 +1,49 @@
+__author__ = 'ykp'
+
+
+import json
+from sklearn.metrics import f1_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.feature_extraction.text import CountVectorizer
+
+data = []
+i = 0
+reviews = []
+labels = []
+features = []
+
+with open('data/yelp_academic_dataset_review.json') as f:
+    for line in f:
+        if i >= 100000:
+            break
+        a = json.loads(line)
+        reviews.append((a['text'],a['stars']))
+        i += 1
+
+corpus = []
+labels = []
+
+for i,j in reviews:
+
+    corpus.append(i)
+
+    if j == 5 or j == 4:
+        labels.append('p')
+    elif j == 3:
+        labels.append('n')
+    else:
+        labels.append('ne')
+
+vectorizer = CountVectorizer(min_df=1,stop_words='english',lowercase=True,max_features=100,ngram_range=(3,1))
+X = vectorizer.fit_transform(corpus).toarray()
+train_data = X[:75000]
+train_labels = labels[:75000]
+test = X[75000:]
+test_labels = labels[75000:]
+clf1 = GaussianNB()
+clf1.fit(train_data,train_labels)
+y_pred = clf1.predict(test)
+accuracy = clf1.score(test,test_labels)
+f1_score(test_labels, y_pred, average='weighted')
+print(accuracy*100)
+print(f1_score(test_labels, y_pred, average='weighted') * 100)
